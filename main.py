da73895a75d916b1322f34234557d9ddf9b649bc
orkur <orkur123@gmail.com>
2024-01-15 15:04:21 +0100

    add hashing

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+from datetime import timedelta, datetime
+from jose import JWTError, jwt
 from fastapi import FastAPI, HTTPException, Depends, Form
-from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from fastapi.security import HTTPBasic, HTTPBasicCredentials, OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from starlette import status
 
 from tables import Team, SessionLocal, User
+from passlib.context import CryptContext
 
 app = FastAPI()
 security = HTTPBasic()
@@ -19,6 +22,12 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+SECRET_KEY = "STRENG GEHEIM"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+password_hasher = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
 def get_db():
@@ -29,13 +38,17 @@ def get_db():
         db.close()
 
 
-def authenticate_user(credentials: HTTPBasicCredentials = Depends(security), db: Session = Depends(get_db)):
-    user = db.query(User).filter(User.username == credentials.username, User.password == credentials.password).first()
-    if user:
-        return user
-    else:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="invalid credentials")
+def create_jwt_token(data: dict, expires_delta: timedelta):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + expires_delta
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
 
+
+def verify_password(plain_password, hashed_password):
+    print(password_hasher.verify(plain_password, hashed_password))
+    return password_hasher.verify(plain_password, hashed_password)
 
 class TeamToInsert(BaseModel):
     name: str
@@ -47,9 +60,19 @@ class UserToInsert(BaseModel):
     password: str
 
 
-@app.get("/group/", dependencies=[Depends(authenticate_user)])
-def get_groups():
-    return {"message": "You are logged in!"}
+@app.post("/login/")
+def login(form: dict, db: Session = Depends(get_db)):
+    username = form.get("username")
+    password = form.get("password")
+    user = db.query(User).filter(User.username == username).first()
+    if not user or not verify_password(password, user.password):
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail="Invalid credentials", headers={"WWW-Authenticate": "Bearer"})
+    user_data = {"sub": user.id, "username": user.username}
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_jwt_token(data=user_data, expires_delta=access_token_expires)
+
+    return {"access_token": access_token, "token_type": "bearer", "message": "User login successfully"}
 
 
 @app.post("/register/")
@@ -58,12 +81,17 @@ def register_user(new_user: UserToInsert, db: Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="wrong username or password")
 
     user = User(username=new_user.username, password=new_user.password)
-    if db.query(User).filter(User.username == user.username).first() is None:
-        db.add(user)
-        db.commit()
-        db.refresh(user)
-        return {"message": "User registered successfully"}
-    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User already exists")
+    if db.query(User).filter(User.username == user.username).first() is not None:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User already exists")
+
+    user.password = password_hasher.hash(new_user.password)
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    user_data = {"sub": user.id, "username": user.username}
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_jwt_token(data=user_data, expires_delta=access_token_expires)
+    return {"access_token": access_token, "token_type": "bearer", "message": "User registered successfully"}
 
 
 @app.post("/team/")
@@ -88,8 +116,8 @@ def get_team(team_id: int, db: Session = Depends(get_db)):
 def get_teams(db: Session = Depends(get_db)):
     teams = db.query(Team).all()
     teams.sort(key=lambda team: team.name)
-    for team in teams:
-        print(team.name, " ", team.description)
+    # for team in teams:
+    #     print(team.name, " ", team.description)
     return teams
 
 
@@ -106,6 +134,6 @@ def delete_team(team_id: int, db: Session = Depends(get_db)):
 
 
 @app.get("/debug/")
-def get_debug_info(db: Session=Depends(get_db)):
+def get_debug_info(db: Session = Depends(get_db)):
     users = db.query(User).all()
     return users
