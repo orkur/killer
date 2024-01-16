6ff8720bb22b43d5697bb7a090b049bdf198cbd8
orkur <orkur123@gmail.com>
2024-01-16 15:27:28 +0100

    team page

--- backend/routers/team.py
+++ backend/routers/team.py
@@ -28,6 +28,12 @@ def get_team(team_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Team has not found")
 
 
+@router.get("/teamName/")
+def get_team_names(team_id: int, db: Session = Depends(get_db)):
+    team = db.query(Team).filter(Team.id == team_id).first()
+    return team.name
+
+
 def get_teams(db: Session = Depends(get_db)):
     teams = db.query(Team).all()
     teams = [team.public for team in teams]
